89d29b733a9d670ce734a398efa29b3756433a96
cahoyn <102632741+cahoyn@users.noreply.github.com>
2023-02-12 21:11:07 -0600

    Add files via upload
    Database microservice for CS-361 project. app.py imports database.py and user interacts with app.py to create, read, update, and delete contacts from the database. Uses Python's built-in sqlite3, no install needed.

---
+++ app.py
@@ -0,0 +1,71 @@
+import database
+
+
+CONTACT_PROMPT = """
+Please select from the following options:
+
+1) Add a new contact.
+2) See all contacts.
+3) Find a contact by name, email, or phone.
+4) Update a contact.
+5) Delete a contact.
+6) Exit.
+
+Your selection: """
+
+SEARCH_PROMPT = """Search by:
+
+1) Name
+2) Email
+3) Phone
+
+Your selection: """
+
+
+def contact():
+    conn = database.create_connection()
+    database.create_table(conn)
+
+    while (user_input := input(CONTACT_PROMPT)) != "6":
+        if user_input == "1":
+            name = input("Enter contact name: ")
+            email = input("Enter contact email: ")
+            phone = input("Enter contact phone number: ")
+
+            database.create_contact(conn, name, email, phone)
+            print("Contact added!")
+        elif user_input == "2":
+            contacts = database.get_all_contacts(conn)
+            for contact in contacts:
+                print(f"{contact[0]} {contact[1]} {contact[2]}")
+        elif user_input == "3":
+            user_input = input(SEARCH_PROMPT)
+            if user_input == "1":
+                name = input("Enter name of contact to find: ")
+                contact = database.get_contact_by_name(conn, name)
+                print(f"{contact[0]} {contact[1]} {contact[2]}")
+            if user_input == "2":
+                email = input("Enter email of contact to find: ")
+                contact = database.get_contact_by_email(conn, email)
+                print(f"{contact[0]} {contact[1]} {contact[2]}")
+            if user_input == "1":
+                phone = input("Enter phone number of contact to find: ")
+                contact = database.get_contact_by_phone(conn, phone)
+                print(f"{contact[0]} {contact[1]} {contact[2]}")
+        elif user_input == "4":
+            name = input("Enter the current name of the contact you'd like to update: ")
+            new_name = input("Enter the updated information. Name: ")
+            email = input("Email: ")
+            phone = input("Phone: ")
+            database.update_contact(conn, name, new_name, email, phone)
+            print("Contact updated!")
+        elif user_input == "5":
+            name = input("Enter the name of the contact you'd like to remove from the database: ")
+            database.delete_contact(conn, name)
+            print("Contact deleted.")
+        else:
+            print("Invalid input, please try again!")
+    conn.close()
+
+
+contact()
---
+++ database.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+print("Contacts Database Program initializing...")
+print("-" * 50)
+
+CREATE_CONTACTS_TABLE = "CREATE TABLE if not exists contacts (name TEXT PRIMARY KEY, email TEXT, phone TEXT);"
+UPDATE_CONTACT = "UPDATE contacts set name = ?, email = ?, phone = ? WHERE name = ?"
+INSERT_CONTACT = "INSERT INTO contacts (name, email, phone) VALUES (?, ?, ?);"
+DELETE_CONTACT = "DELETE FROM contacts WHERE name = ?"
+GET_ALL_CONTACTS = "SELECT * FROM contacts;"
+GET_CONTACT_BY_NAME = "SELECT * FROM contacts WHERE name = ?;"
+GET_CONTACT_BY_EMAIL = "SELECT * FROM contacts WHERE email = ?;"
+GET_CONTACT_BY_PHONE = "SELECT * FROM contacts WHERE phone = ?;"
+
+
+def create_connection():
+    """Create a database connection"""
+    conn = None
+    try:
+        conn = sqlite3.connect('contacts.db')
+    except sqlite3.Error as e:
+        print(e)
+
+    return conn
+
+
+def create_contact(conn, name, email, phone):
+    """Create a new contact"""
+    with conn:
+        conn.execute(INSERT_CONTACT, (name, email, phone))
+
+
+def create_table(conn):
+    """Create contacts table in database"""
+    with conn:
+        conn.execute(CREATE_CONTACTS_TABLE)
+
+
+def update_contact(conn, contact_name, new_name, email, phone):
+    """Update an existing contact"""
+    cur = conn.cursor()
+    data = (new_name, email, phone, contact_name)
+    cur.execute(UPDATE_CONTACT, data)
+    conn.commit()
+
+
+def delete_contact(conn, contact_name):
+    """Delete a contact from the database"""
+    cur = conn.cursor()
+    cur.execute(DELETE_CONTACT, contact_name)
+    conn.commit()
+
+
+def get_all_contacts(conn):
+    """Read all contacts in table"""
+    with conn:
+        return conn.execute(GET_ALL_CONTACTS).fetchall()
+
+
+def get_contact_by_name(conn, name):
+    """Read specific contact information"""
+    with conn:
+        return conn.execute(GET_CONTACT_BY_NAME, name).fetchall()
+
+
+def get_contact_by_email(conn, email):
+    """Read specific contact information"""
+    with conn:
+        return conn.execute(GET_CONTACT_BY_EMAIL, email).fetchall()
+
+
+def get_contact_by_phone(conn, phone):
+    """Read specific contact information"""
+    with conn:
+        return conn.execute(GET_CONTACT_BY_PHONE, phone).fetchall()
+
+
