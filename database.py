b4d17923dab58651aec6a52368e9cccbb931c371
cahoyn <102632741+cahoyn@users.noreply.github.com>
2023-02-20 19:42:22 -0600

    Update database.py
    Made corrections to the get_contact_by functions

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ print("Contacts Database Program initializing...")
 print("-" * 50)
 
 CREATE_CONTACTS_TABLE = "CREATE TABLE if not exists contacts (name TEXT PRIMARY KEY, email TEXT, phone TEXT);"
-UPDATE_CONTACT = "UPDATE contacts set name = ?, email = ?, phone = ? WHERE name = ?"
+UPDATE_CONTACT = "UPDATE contacts set name = ?, email = ?, phone = ? WHERE name = ?;"
 INSERT_CONTACT = "INSERT INTO contacts (name, email, phone) VALUES (?, ?, ?);"
-DELETE_CONTACT = "DELETE FROM contacts WHERE name = ?"
+DELETE_CONTACT = "DELETE FROM contacts WHERE name = ?;"
 GET_ALL_CONTACTS = "SELECT * FROM contacts;"
 GET_CONTACT_BY_NAME = "SELECT * FROM contacts WHERE name = ?;"
 GET_CONTACT_BY_EMAIL = "SELECT * FROM contacts WHERE email = ?;"
@@ -47,7 +47,7 @@ def update_contact(conn, contact_name, new_name, email, phone):
 def delete_contact(conn, contact_name):
     """Delete a contact from the database"""
     cur = conn.cursor()
-    cur.execute(DELETE_CONTACT, contact_name)
+    cur.execute(DELETE_CONTACT, (contact_name,))
     conn.commit()
 
 
@@ -60,18 +60,18 @@ def get_all_contacts(conn):
 def get_contact_by_name(conn, name):
     """Read specific contact information"""
     with conn:
-        return conn.execute(GET_CONTACT_BY_NAME, name).fetchall()
+        return conn.execute(GET_CONTACT_BY_NAME, (name,)).fetchall()
 
 
 def get_contact_by_email(conn, email):
     """Read specific contact information"""
     with conn:
-        return conn.execute(GET_CONTACT_BY_EMAIL, email).fetchall()
+        return conn.execute(GET_CONTACT_BY_EMAIL, (email,)).fetchall()
 
 
 def get_contact_by_phone(conn, phone):
     """Read specific contact information"""
     with conn:
-        return conn.execute(GET_CONTACT_BY_PHONE, phone).fetchall()
+        return conn.execute(GET_CONTACT_BY_PHONE, (phone,)).fetchall()
 
 
